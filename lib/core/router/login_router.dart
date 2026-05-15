3043746cb1e74bfb4c410fbbdc10fa34ab169a7f
davit412v2 <luisdavidbarrigagaray97@gmail.com>
2026-05-15 17:35:15 -0500

    Implement routing with GoRouter and create Home, Login, and Task pages

---
+++ lib/core/router/login_router.dart
@@ -0,0 +1,9 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+GoRoute loginRoute = GoRoute(
+  path: "/login",
+  builder: (context, state) => const Center(),
+);
---
+++ lib/core/router/task_router.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+GoRoute taskRoute = GoRoute(
+  path: "/task",
+  builder: (context, state) => const Center(),
+);
